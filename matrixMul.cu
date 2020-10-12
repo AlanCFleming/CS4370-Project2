591cd9f135c68ab3406627db6b3f1e086eebbcae
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-10-12 00:53:33 -0400

    fixed loop variable

--- matrixMul.cu
+++ matrixMul.cu
@@ -8,7 +8,7 @@ void mul_matrix_cpu(float *M, float *N, float *P, int width){
 	for( int i = 0; i<width; i++){
 		for( int j = 0; j<width; j++){
 			float sum = 0;
-			for (float k = 0; k < width; k++){
+			for (int k = 0; k < width; k++){
 				sum += M[i * width + k] * N[k * width + j];
 			}
 			P[i * width + j] = sum;
