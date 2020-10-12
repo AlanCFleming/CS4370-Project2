7d84eaf462df008d82ba3be9fb8746d846506ad6
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-10-12 01:09:58 -0400

    Fixed copying to shared memory

--- matrixMul.cu
+++ matrixMul.cu
@@ -34,8 +34,8 @@ __global__ void mul_matrix_gpu(float *M, float *N, float *P, int width) {
 
 	for (int i = 0; i < (width / blockDim.x); ++i) {
 		//copy global memory into shared memory
-		ds_M[threadIdx.y][threadIdx.x] = M[row * width + i * blockDim.x + threadIdx.x];
-		ds_N[threadIdx.y][threadIdx.x] = N[col + (i * blockDim.x + threadIdx.y) * width];		
+		ds_M[row  * width + col] = M[row * width + i * blockDim.x + threadIdx.x];
+		ds_N[row * width + col] = N[col + (i * blockDim.x + threadIdx.y) * width];		
 		//ensure all data is copied
 		__synchthreads();
 		
